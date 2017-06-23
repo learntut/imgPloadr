c94101e7ca374c9f2ba13c5b32ba6138c3984970
Shyam S <shyam.s@thinkpalm.com>
2017-06-23 16:34:19 +0530

    local commit

--- server/configure.js
+++ server/configure.js
@@ -6,21 +6,30 @@ bodyParser = require('body-parser'),
 cookieParser = require('cookie-parser'),
 morgan = require('morgan'),
 errorHandler = require('errorhandler');
+moment = require('moment');
+multer = require('multer');
 module.exports = function(app) {
 	app.use(morgan('dev'));
-	app.use(bodyParser({
-	uploadDir:path.join(__dirname, 'public/upload/temp')
-}));
-app.use(cookieParser('some-secret-value-here'));
-routes(app);
-app.use('/public/', express.static(path.join(__dirname,'../public')));
-app.engine('handlebars', exphbs.create({
+	// app.use(bodyParser({
+	// 	uploadDir:path.join(__dirname, 'public/upload/temp')
+	// }));
+	app.use(multer({ dest: path.join(__dirname,'public/upload/temp')}).single('singleInputFileName'));
+	app.use(cookieParser('some-secret-value-here'));
+	routes(app);
+
+	app.use('/public/', express.static(path.join(__dirname,'../public')));
+	app.engine('handlebars', exphbs.create({
 	defaultLayout: 'main',
 	layoutsDir: app.get('views') + '/layouts',
-	partialsDir: [app.get('views') + '/partials']
+	partialsDir: [app.get('views') + '/partials'],
+	helpers: {
+			timeago: function(timestamp) {
+			return moment(timestamp).startOf('minute').fromNow();
+			}
+		}
 	}).engine);
-app.set('view engine', 'handlebars');
-if ('development' === app.get('env')) {
+	app.set('view engine', 'handlebars');
+	if ('development' === app.get('env')) {
 		app.use(errorHandler());
 	}
 	return app;
